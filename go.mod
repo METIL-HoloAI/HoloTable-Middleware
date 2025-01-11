5ca5f375aabc0b72417d305a4072322bddbc9bc2
Enrique Romero <enrique.romero1110@gmail.com>
2025-01-11 14:23:02 -0500

    Intialize repository

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/METIL-HoloAI/HoloTable-Middleware
+
+go 1.23.4
---
+++ internal/cmd/cli/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    fmt.Println("Hello, World!")
+
+    // Load yaml
+
+    // Check how user wants to listen for input
+    // and start that listener
+}
