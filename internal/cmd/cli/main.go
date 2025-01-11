26fb46d0d9b7185df0b7e96a3ccce63941ee2c97
Enrique Romero <enrique.romero1110@gmail.com>
2025-01-11 17:51:31 -0500

    Add loading general.yaml and displaying listener choice

--- internal/cmd/cli/main.go
+++ internal/cmd/cli/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/configloader"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello World2!")
-
 	// Load yaml
+	settings, err := configloader.LoadGeneral()
+	if err != nil {
+		fmt.Println("Error loading general settings")
+		fmt.Println(err)
+		return
+	}
 
 	// Check how user wants to listen for input
 	// and start that listener
+	if(settings.Listener == "cli") {
+		fmt.Println("CLI Listener")
+	} else if(settings.Listener == "text") {
+		fmt.Println("Text Listener")
+	} else {
+		fmt.Println("Invalid listener option in general.yaml")
+	}
+
 }
