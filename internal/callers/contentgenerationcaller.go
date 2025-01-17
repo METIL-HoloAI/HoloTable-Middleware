eb57e76e6dc26c072750f63e248edc38bc0b4466
Jacob Stella <ja416731@ucf.edu>
2025-01-17 18:51:53 -0500

    add folder for contentgen yaml files and update loader

---
+++ internal/callers/contentgenerationcaller.go
@@ -0,0 +1,7 @@
+package callers
+
+import(
+	"fmt"
+)
+
+func 
--- internal/configloader/loader.go
+++ internal/configloader/loader.go
@@ -36,3 +36,59 @@ func GetIntentDetection() (structs.IntentDetectionSettings, error) {
 
 	return settings, nil
 }
+
+func GetImageGeneration() (structs.APIConfig, error) {
+	file, err := os.ReadFile("config/contentgeneration/imagegeneration.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
+
+func GetVideoGeneration() (structs.APIConfig, error) {
+	file, err := os.ReadFile("config/contentgeneration/videogeneration.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
+
+func GetGifGeneration() (structs.APIConfig, error) {
+	file, err := os.ReadFile("config/contentgeneration/gifgeneration.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
+
+func Get3dGeneration() (structs.APIConfig, error) {
+	file, err := os.ReadFile("config/contentgeneration/3dgeneration.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
