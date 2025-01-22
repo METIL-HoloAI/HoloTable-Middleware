4c8a4a4ed1f491dfac25862cea7d62b5f8b25da6
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-01-22 15:00:00 -0500

    add InitDatabase call to main

--- cmd/holoai/main.go
+++ cmd/holoai/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 
 	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/configloader"
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/database"
 	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/listeners"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -19,12 +18,7 @@ func main() {
 		return
 	}
 
-	// Load database
-	db, err := sql.Open("sqlite3", settings.DataDir+"/db.db")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
+	database.InitDatabase()
 
 	// Check how user wants to listen for input
 	// and start that listener
