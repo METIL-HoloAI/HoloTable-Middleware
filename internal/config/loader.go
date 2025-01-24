441ba154aa3540a4fe5794bc366ea3002b34c29f
Enrique Romero <enrique.romero1110@gmail.com>
2025-01-23 20:47:11 -0500

    Add configs are now loaded globaly and can be accessed from any file instead of having to load the config for every call

---
+++ internal/config/loader.go
@@ -0,0 +1,186 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/config/structs"
+	"gopkg.in/yaml.v3"
+)
+
+var General structs.GeneralSettings
+var IntentDetection structs.IntentDetectionSettings
+var ImageGen structs.APIConfig
+var VideoGen structs.APIConfig
+var GifGen structs.APIConfig
+var ModelGen structs.APIConfig
+
+func LoadYaml() {
+	var err error
+	General, err = getGeneral()
+	if err != nil {
+		log.Fatal("Error parsing general.yaml: ", err)
+	}
+
+	IntentDetection, err = getIntentDetection()
+	if err != nil {
+		log.Fatal("Error parsing intentdetection.yaml: ", err)
+	}
+
+	ImageGen, err = getImage()
+	if err != nil {
+		log.Fatal("Error parsing imagegen.yaml: ", err)
+	}
+
+	VideoGen, err = getVideo()
+	if err != nil {
+		log.Fatal("Error parsing videogen.yaml: ", err)
+	}
+
+	GifGen, err = getGif()
+	if err != nil {
+		log.Fatal("Error parsing gifgen.yaml: ", err)
+	}
+
+	ModelGen, err = get3d()
+	if err != nil {
+		log.Fatal("Error parsing 3dgen.yaml: ", err)
+	}
+}
+
+// This is a workaround to make sure filepaths are always pulled relative
+// to loader.go
+func getConfigPath(relativePath string) (string, error) {
+	// the caller will return back the file that called this function
+	// the way this is set up is that it'll always be a function within
+	// loader.go, making sure that filepaths remain consistent
+	_, loaderfile, _, ok := runtime.Caller(0)
+	if !ok {
+		return "", fmt.Errorf("unable to retrieve caller information")
+	}
+
+	// Get the directory where loader.go is located
+	loaderDir := filepath.Dir(loaderfile)
+
+	configPath := filepath.Join(loaderDir, "../../config", relativePath)
+	configPath = filepath.Clean(configPath)
+
+	return configPath, nil
+}
+
+func getGeneral() (structs.GeneralSettings, error) {
+	configPath, err := getConfigPath("general.yaml")
+	if err != nil {
+		return structs.GeneralSettings{}, err
+	}
+
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		return structs.GeneralSettings{}, err
+	}
+
+	var settings structs.GeneralSettings
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.GeneralSettings{}, err
+	}
+
+	return settings, nil
+}
+
+func getIntentDetection() (structs.IntentDetectionSettings, error) {
+	configPath, err := getConfigPath("intentdetection.yaml")
+	if err != nil {
+		return structs.IntentDetectionSettings{}, err
+	}
+
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		return structs.IntentDetectionSettings{}, err
+	}
+
+	var settings structs.IntentDetectionSettings
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.IntentDetectionSettings{}, err
+	}
+
+	return settings, nil
+}
+
+func getImage() (structs.APIConfig, error) {
+	configPath, err := getConfigPath("imagegen.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
+
+func getVideo() (structs.APIConfig, error) {
+	configPath, err := getConfigPath("videogen.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
+
+func getGif() (structs.APIConfig, error) {
+	configPath, err := getConfigPath("gifgen.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
+
+func get3d() (structs.APIConfig, error) {
+	configPath, err := getConfigPath("3dgen.yaml")
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	var settings structs.APIConfig
+	if err := yaml.Unmarshal(file, &settings); err != nil {
+		return structs.APIConfig{}, err
+	}
+
+	return settings, nil
+}
