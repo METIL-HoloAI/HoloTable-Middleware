13468f411b69c502846aac7948878abc319e22d1
Enrique Romero <enrique.romero1110@gmail.com>
2025-01-27 18:50:14 -0500

    Remove debug statements

--- cmd/holoai/main.go
+++ cmd/holoai/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/config"
 	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/database"
@@ -14,11 +13,6 @@ func main() {
 	config.LoadYaml()
 
 	database.Init()
-
-	// Test print
-	imageGenkey := os.Getenv("IMAGE_API_KEY")
-	fmt.Println("in main: Image API Key:" + imageGenkey)
-	//
 
 	// Check how user wants to listen for input
 	// and start that listener
