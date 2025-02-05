112cb7d47b39a1c7c7ce0276e068ad09446e94fb
Jacob Stella <ja416731@ucf.edu>
2025-02-05 13:35:50 -0500

    update func name for clarity

--- internal/callers/loadprompt.go
+++ internal/callers/loadprompt.go
@@ -104,26 +104,26 @@ func LoadPrompt(prompt string) ([]byte, error) {
 // ////
 func BuildPayload(initPrompt, userPrompt string) (map[string]interface{}, error) {
 	payloadConfig := config.IntentDetection.Payload
-	payload := deepReplace(payloadConfig, initPrompt, userPrompt).(map[string]interface{})
+	payload := searchPayload(payloadConfig, initPrompt, userPrompt).(map[string]interface{})
 	return payload, nil
 }
 
 // Recursively searches the payload for the initialPronpt and userPrompt placeholders within our intent detection struct and replaces them with the actual values
 // can handle any data structure
-func deepReplace(data interface{}, initPrompt, userPrompt string) interface{} {
+func searchPayload(data interface{}, initPrompt, userPrompt string) interface{} {
 	switch v := data.(type) {
 	case string: //if the data is a string, it it searches and replaces the placeholders with the actual values
 		return replacePlaceholders(v, initPrompt, userPrompt)
 	case map[string]interface{}: //creates a new map to store the modified key-value pairs then traverses the map in search of placeholders
 		newMap := make(map[string]interface{})
 		for key, value := range v {
-			newMap[key] = deepReplace(value, initPrompt, userPrompt)
+			newMap[key] = searchPayload(value, initPrompt, userPrompt)
 		}
 		return newMap
 	case []interface{}: // create a new list to store the modified values then traverses the list in search of placeholders
 		newList := make([]interface{}, len(v))
 		for i, item := range v {
-			newList[i] = deepReplace(item, initPrompt, userPrompt)
+			newList[i] = searchPayload(item, initPrompt, userPrompt)
 		}
 		return newList
 	default: //if the data is not a string, map, or list, it is returned as is
@@ -138,6 +138,7 @@ func replacePlaceholders(text, initPrompt, userPrompt string) string {
 	return text
 }
 
+// REMOVE AFTER TESTING
 // Helper function to pretty-print JSON output
 func prettyPrint(data interface{}) {
 	jsonBytes, err := json.MarshalIndent(data, "", "  ")
