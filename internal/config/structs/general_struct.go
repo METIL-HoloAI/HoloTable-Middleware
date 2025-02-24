cca808bb9f90c2da6c0e74858adb1df31733c2cb
Enrique Romero <enrique.romero1110@gmail.com>
2025-02-23 23:25:01 -0500

    Remove microphone and text toggle

--- internal/config/structs/general_struct.go
+++ internal/config/structs/general_struct.go
@@ -3,7 +3,6 @@ package structs
 import _ "gopkg.in/yaml.v3"
 
 type GeneralSettings struct {
-	Listener      string `yaml:"listener"`
 	DataDir       string `yaml:"dataDir"`
 	OpenWebsocket bool   `yaml:"openWebsocket"`
 }
