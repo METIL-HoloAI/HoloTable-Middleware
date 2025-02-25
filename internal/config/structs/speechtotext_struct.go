fd74def087eced28f42ca1df6bb4f19634531b80
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-02-24 19:40:00 -0500

    update stt config for whispher

--- internal/config/structs/speechtotext_struct.go
+++ internal/config/structs/speechtotext_struct.go
@@ -3,6 +3,11 @@ package structs
 import _ "gopkg.in/yaml.v3"
 
 type SpeechToTextSettings struct {
-	WebsocketURL string `yaml:"WebsocketURL"`
-	Keyword      string `yaml:"Keyword"`
+	WebsocketURL string                 `yaml:"WebsocketURL"`
+	Keyword      string                 `yaml:"Keyword"`
+	Endpoint     string                 `yaml:"endpoint"`
+	Method       string                 `yaml:"method"`
+	Headers      map[string]string      `yaml:"headers"`
+	Payload      map[string]interface{} `yaml:"payload"`
+	ResponsePath string                 `yaml:"responsePath"`
 }
