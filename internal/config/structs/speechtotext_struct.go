3f5e4278df3a24c0fbba142bf773db6383ca2a68
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-02-25 23:17:14 -0500

    rework structs for new config yaml

--- internal/config/structs/speechtotext_struct.go
+++ internal/config/structs/speechtotext_struct.go
@@ -3,11 +3,16 @@ package structs
 import _ "gopkg.in/yaml.v3"
 
 type SpeechToTextSettings struct {
-	WebsocketURL string                 `yaml:"WebsocketURL"`
-	Keyword      string                 `yaml:"Keyword"`
-	Endpoint     string                 `yaml:"endpoint"`
-	Method       string                 `yaml:"method"`
-	Headers      map[string]string      `yaml:"headers"`
-	Payload      map[string]interface{} `yaml:"payload"`
-	ResponsePath string                 `yaml:"responsePath"`
+	LiveTranscription struct {
+		WebsocketURL string `yaml:"websocketURL"`
+		Keyword      string `yaml:"keyword"`
+	} `yaml:"liveTranscription"`
+
+	SpeechToTextAPI struct {
+		Endpoint     string                 `yaml:"endpoint"`
+		Method       string                 `yaml:"method"`
+		Headers      map[string]string      `yaml:"headers"`
+		Payload      map[string]interface{} `yaml:"payload"`
+		ResponsePath string                 `yaml:"responsePath"`
+	} `yaml:"speechToTextAPI"`
 }
