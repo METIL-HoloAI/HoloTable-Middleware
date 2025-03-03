e40db724226d6985514ceb3f68c173d4275b3f99
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-03-03 13:26:10 -0500

    scaffold sveltekit project

---
+++ internal/callers/loadtext.go
@@ -0,0 +1,32 @@
+package callers
+
+// use jsonData, err := callers.LoadAudio(prompt) to call this function
+
+import "fmt"
+
+// "bytes"
+// "encoding/json"
+// "fmt"
+// "io"
+// "net/http"
+// "mime/multiport"
+// "os"
+// "path/filepath"
+
+// "github.com/METIL-HoloAI/HoloTable-Middleware/internal/config"
+
+// given the filepath of the audio file, send the audio file and return a byteslice of the response
+// func sendAudioFile(audioFilePath string) ([]byte, error) {
+// 	// open and read the saved audio file
+// 	// send as a request- perhaps as multipart/form-data to the STT api
+// 	fmt.Println("test")
+
+// }
+
+// given the filepath of the audio file, send the audio file and return a byteslice of the response
+func sendAudioFil2e(audioFilePath string) {
+	// open and read the saved audio file
+	// send as a request- perhaps as multipart/form-data to the STT api
+	fmt.Println("test")
+
+}
