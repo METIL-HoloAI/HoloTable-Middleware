86ad40778062b4ba2baf7e7ef9db6c09b13a462f
Ana L. La Rosa Perez <analaura.larosap@gmail.com>
2025-03-03 08:38:36 -0500

    changed structure to use filepath instead of filedata

--- internal/unityserver/assetstruct/asset_struct.go
+++ internal/unityserver/assetstruct/asset_struct.go
@@ -4,5 +4,5 @@ type AssetMessage struct {
 	Type      string `json:"type"`
 	Name      string `json:"name"`
 	Extension string `json:"extension"`
-	FileData  []byte `json:"fileData,omitempty"`
+	FilePath  string `json:"filePath,omitempty"`
 }
