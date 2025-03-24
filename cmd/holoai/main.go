eca2b083c41bd928b9a396cf95141790c9f4c0c5
Ana <102564909+Wasasita@users.noreply.github.com>
2025-03-24 10:50:47 -0400

    Added structure for handling data bytes and logic for using the correct type depending on what the UnityClient uses

--- cmd/holoai/main.go
+++ cmd/holoai/main.go
@@ -1,42 +1,44 @@
 package main
 
 import (
-	// "database/sql"
-	// "os"
+	"database/sql"
+	"os"
 
-	// "github.com/METIL-HoloAI/HoloTable-Middleware/internal/config"
-	// "github.com/METIL-HoloAI/HoloTable-Middleware/internal/database"
-	// "github.com/METIL-HoloAI/HoloTable-Middleware/internal/endpoints/restapi"
-	// "github.com/METIL-HoloAI/HoloTable-Middleware/internal/endpoints/websocket"
-	// "github.com/METIL-HoloAI/HoloTable-Middleware/internal/listeners"
 	"log"
-	"time"
+
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/config"
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/database"
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/endpoints/restapi"
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/endpoints/websocket"
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/listeners"
+
+	// "time"
 
 	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/unityserver"
-	// "github.com/METIL-HoloAI/HoloTable-Middleware/internal/utils"
-	// _ "github.com/mattn/go-sqlite3"
-	// "github.com/sirupsen/logrus"
+	"github.com/METIL-HoloAI/HoloTable-Middleware/internal/utils"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
-	// // Load configuration
-	// config.LoadYaml()
+	// Load configuration
+	config.LoadYaml()
 
-	// // Initialize logger
-	// config.InitLogger()
+	// Initialize logger
+	config.InitLogger()
 
-	// if err := os.MkdirAll(config.General.DataDir, os.ModePerm); err != nil {
-	// 	logrus.Fatal("Failed to create data directory:", err)
-	// }
+	if err := os.MkdirAll(config.General.DataDir, os.ModePerm); err != nil {
+		logrus.Fatal("Failed to create data directory:", err)
+	}
 
-	// db, err := sql.Open("sqlite3", config.General.DataDir+"database.db?_mode=shared&_journal_mode=WAL")
-	// if err != nil {
-	// 	logrus.Fatal("Failed to open database:", err)
-	// }
-	// defer db.Close()
+	db, err := sql.Open("sqlite3", config.General.DataDir+"database.db?_mode=shared&_journal_mode=WAL")
+	if err != nil {
+		logrus.Fatal("Failed to open database:", err)
+	}
+	defer db.Close()
 
-	// database.Init(db)
+	database.Init(db)
 
 	// Start WebSocket server
 	go unityserver.StartWebSocketServer()
@@ -44,25 +46,25 @@ func main() {
 	<-unityserver.ClientReady
 	log.Println("Unity client connected")
 
-	unityserver.ExportAsset("catLion", "PNG", "../test/catLion.PNG")
-	time.Sleep(3 * time.Second)
-	unityserver.ExportAsset("table", "glb", "../test/table.glb")
-	time.Sleep(5 * time.Second)
-	unityserver.ExportAsset("movingImage", "mp4", "../test/movingImage.mp4")
-	time.Sleep(5 * time.Second)
-	unityserver.ExportAsset("Dragonflying2", "mp4", "../test/Dragonflying2.mp4")
-	time.Sleep(5 * time.Second)
-	unityserver.ExportAsset("Cat2", "mp4", "../test/Cat2.mp4")
-	time.Sleep(5 * time.Second)
-	unityserver.ExportAsset("cat", "mp4", "../test/cat.mp4")
-	time.Sleep(5 * time.Second)
-	// unityserver.ExportAsset(fileName, extension, filePath) // HOW TO call my function
+	// Manual TESTING of UnityServer
+	// unityserver.ExportAsset("catLion", "PNG", "../test/catLion.PNG")
+	// time.Sleep(3 * time.Second)
+	// unityserver.ExportAsset("table", "glb", "../test/table.glb")
+	// time.Sleep(5 * time.Second)
+	// unityserver.ExportAsset("movingImage", "mp4", "../test/movingImage.mp4")
+	// time.Sleep(5 * time.Second)
+	// unityserver.ExportAsset("Dragonflying2", "mp4", "../test/Dragonflying2.mp4")
+	// time.Sleep(5 * time.Second)
+	// unityserver.ExportAsset("Cat2", "mp4", "../test/Cat2.mp4")
+	// time.Sleep(5 * time.Second)
+	// unityserver.ExportAsset("cat", "mp4", "../test/cat.mp4")
+	// time.Sleep(5 * time.Second)
 
-	// if config.General.OpenWebsocket {
-	// 	go websocket.EstablishConnection()
-	// 	restapi.StartRestAPI()
-	// 	utils.WaitForInterrupt()
-	// } else {
-	// 	listeners.StartTextListener()
-	// }
+	if config.General.OpenWebsocket {
+		go websocket.EstablishConnection()
+		restapi.StartRestAPI()
+		utils.WaitForInterrupt()
+	} else {
+		listeners.StartTextListener()
+	}
 }
